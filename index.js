f83f92c1e678b91ab10fb115ae68457b9b667bcd
“Meroni-bcg” <Amairani.b.cervantes@gmail.com>
2022-10-13 23:04:26 -0700

    added username questions and answers

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your Code Here
+// Get and remember the user's name 
+let userName = window.prompt('Please enter your name:')
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+if(userAnswer === question.correctAnswer){
+    console.log('correct')
+    }
+}
+// Remember the user's score
+
+
